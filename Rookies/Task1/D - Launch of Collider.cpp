a1720b040dc1745492f87866b59063cb9c50c5a2
Moosayed1 <mohamed1212213183@gmail.com>
2024-11-30 17:00:08 +0200

    last problem

---
+++ Rookies/Task1/D - Launch of Collider.cpp
@@ -0,0 +1,85 @@
+// ﷽
+// Contest: Get Ready
+// Judge: Virtual Judge
+// URL: https://vjudge.net/contest/676467#problem/D
+// Memory Limit: 256
+// Time Limit: 2000
+// Start: Sat 30 Nov 2024 02:36:17 PM EET
+//
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+namespace __gnu_pbds{
+    typedef tree<long long,
+            null_type,
+            less<long long>,
+    rb_tree_tag,
+    tree_order_statistics_node_update> ordered_set; 
+}
+using namespace __gnu_pbds;
+#ifdef MOHAMED
+#include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
+
+#define int long long
+#define all(a) (a).begin(), (a).end()
+#define sz(a) (int)(a).size()
+#define pb push_back
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define OO 2e9
+#define endl "\n"
+#define popCnt(x) (__builtin_popcountll(x))
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1};
+const int dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+
+void solve() {
+  int n;cin>>n;
+  string s;cin>>s;
+  vi arr(n);cin>>arr;
+  int ans=-1;
+  for (int i = 0; i < n-1; i++) {
+    if(s[i]=='R'&&s[i+1]=='L'){
+      int time=(arr[i+1]-arr[i])/2;
+      if(ans==-1||time<ans)
+        ans=time;
+    }
+  }
+  cout<<ans<<endl;
+}
+int32_t main() {
+
+  //  freopen("whereami.in", "r", stdin);
+  //  freopen("whereami.out", "w", stdout);
+  fastio();
+  int t = 1;
+  // cin>>t;
+  while (t--)
+    solve();
+  return 0;
+}
